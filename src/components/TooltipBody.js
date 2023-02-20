d3a4340c6e2f19dee17193b873024b3d845c5622
Piyush Yadav <piyushyadav1105@gmail.com>
2023-02-20 10:46:59 +0530

    final push

--- src/components/TooltipBody.js
+++ src/components/TooltipBody.js
@@ -3,74 +3,76 @@ import { React, useState } from "react";
 import { ThumbUpSharp } from "@mui/icons-material";
 import Tooltip from "@mui/material/Tooltip";
 import { ReactionBarSelector } from "@charkour/react-reactions";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import TagFacesIcon from "@mui/icons-material/TagFaces";
-import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
-import SentimentNeutralIcon from "@mui/icons-material/SentimentNeutral";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import like from "../images/like.png";
+import angry from "../images/angry.png";
+import haha from "../images/haha.png";
+import heart from "../images/heart.png";
+import sad from "../images/sad.png";
+import surprise from "../images/surprise.png";
 
+import "./card.css";
 const TooltipBody = () => {
-  const handleChange = (e) => {
+  const handleChange = () => {
     setIcon("");
   };
-
   const [icon, setIcon] = useState("");
-
+  console.log(icon);
   const [open, setOpen] = useState(false);
-
   const handleTooltipClose = () => {
     setOpen(false);
   };
-
   const handleTooltipOpen = () => {
     setOpen(true);
   };
   return (
     <>
       <div>
-        <Tooltip
-          PopperProps={{
-            disablePortal: true,
-          }}
-          sx={{ bgcolor: "white" }}
-          title={
-            <ReactionBarSelector
-              style={{
-                width: "100%",
-              }}
-              onSelect={(e) => setIcon(e)}
-            />
-          }
-          onClose={() => handleTooltipClose()}
-          open={open}
-          disableFocusListener
-          disableHoverListener
-          disableTouchListener
-        >
-          <IconButton
-            onClick={(e) => {
-              handleChange(e);
-              handleTooltipOpen();
+        <ClickAwayListener onClickAway={handleTooltipClose}>
+          <Tooltip
+            PopperProps={{
+              disablePortal: true,
             }}
-            aria-label="emoji"
+            sx={{ bgcolor: "white" }}
+            title={
+              <ReactionBarSelector
+                style={{
+                  width: "100%",
+                }}
+                onSelect={(e) => setIcon(e)}
+              />
+            }
+            onClose={handleTooltipClose}
+            open={open}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
           >
-            {icon === "satisfaction" ? (
-              <ThumbUpSharp color="primary" />
-            ) : icon === "love" ? (
-              <FavoriteIcon color="error" />
-            ) : icon === "happy" ? (
-              <TagFacesIcon color="primary" />
-            ) : icon === "surprise" ? (
-              <EmojiEmotionsIcon color="error" />
-            ) : icon === "sad" ? (
-              <SentimentVeryDissatisfiedIcon color="warning" />
-            ) : icon === "angry" ? (
-              <SentimentNeutralIcon color="warning" />
-            ) : (
-              <ThumbUpSharp />
-            )}
-          </IconButton>
-        </Tooltip>
+            <IconButton
+              onClick={() => {
+                handleChange();
+                handleTooltipOpen();
+              }}
+              aria-label="emoji"
+            >
+              {icon === "satisfaction" ? (
+                <img className="emoji-img" src={like} alt="like" />
+              ) : icon === "love" ? (
+                <img className="emoji-img" src={heart} alt="heart" />
+              ) : icon === "happy" ? (
+                <img className="emoji-img" src={haha} alt="haha" />
+              ) : icon === "surprise" ? (
+                <img className="emoji-img" src={surprise} alt="surprise" />
+              ) : icon === "sad" ? (
+                <img className="emoji-img" src={sad} alt="sad" />
+              ) : icon === "angry" ? (
+                <img className="emoji-img" src={angry} alt="angry" />
+              ) : (
+                <ThumbUpSharp />
+              )}
+            </IconButton>
+          </Tooltip>
+        </ClickAwayListener>
       </div>
     </>
   );
